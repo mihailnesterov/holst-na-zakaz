bcb3998dd6096f8cd205e4dc5ce595fecacff651
mihail <mhause@mail.ru>
2019-11-07 01:07:09 +0700

    fix: poster widget class psr-2 standard

--- modules/admin/components/poster/PosterWidget.php
+++ modules/admin/components/poster/PosterWidget.php
@@ -1,18 +1,22 @@
 <?php
 namespace app\modules\admin\components\poster;
+
 use Yii;
 use yii\base\Widget;
 use yii\web\UploadedFile;
 use app\models\Posters;
 
-class PosterWidget extends Widget {
-    
+class PosterWidget extends Widget 
+{ 
     public $id = null;
     
     public function run() {   
         parent::run();
+        
         $model = $this->id ? Posters::findOne($this->id) : new Posters();
+        
         $this->view->title = $this->id ? 'Картина: '.$model->articul : 'Добавить картину ...';
+        
         if( $model->load(Yii::$app->request->post()) && $model->validate()) { 
             if($model->save()) {   
                 $model->imageFile = UploadedFile::getInstance($model, 'imageFile');
@@ -22,10 +26,10 @@ class PosterWidget extends Widget {
                 return;
             }
         }
+
         return $this->render('poster', [
             'model' => $model,
         ]);
         
     }
 }
-?>
