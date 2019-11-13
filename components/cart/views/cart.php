f5b222a0e3adbd6a2ad20848457115a2ca6a2d8c
mihail <mhause@mail.ru>
2019-11-14 05:15:28 +0700

    fix: cart widget - cartCount badge style

--- components/cart/views/cart.php
+++ components/cart/views/cart.php
@@ -8,7 +8,7 @@ use yii\helpers\Html;
     <?= Html::a(
         '<i class="fa fa-shopping-cart uk-margin-small-right"></i>
         Корзина 
-        <span id="cartCount" class="uk-margin-small-left">0</span> шт.
+        <span id="cartCount" class="uk-margin-small-left uk-badge">0</span>
         <span id="cartSum" class="uk-hidden">0.00</span>',
         Url::to('cart'),
         [ 'class' => 'uk-button uk-button-default uk-margin-small-top' ]
