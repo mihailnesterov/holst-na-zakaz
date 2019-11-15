10ba3cccdfe55508d4804c0421a9cc5689eaae59
mihail <mhause@mail.ru>
2019-11-16 06:53:54 +0700

    fix: PosterListImageWidget del comments

--- components/posterlistimage/PosterListImageWidget.php
+++ components/posterlistimage/PosterListImageWidget.php
@@ -1,9 +1,7 @@
 <?php
 namespace app\components\posterlistimage;
 use yii\base\Widget;
-//use app\models\Images;
 use app\models\Posters;
-use yii\helpers\Url;
 
 class PosterListImageWidget extends Widget 
 {
@@ -13,13 +11,10 @@ class PosterListImageWidget extends Widget
         $bundle = PosterListImageAsset::register( $this->getView() );
         parent::run();
         if($this->poster_id){
-            //$image = Images::find()->where(['poster_id' => $this->poster_id])->one();
             $poster = \Yii::$app->cache->getOrSet('poster', function () {
                 return Posters::findOne(['id' => $this->poster_id]);
             }, 3600);
-            //$poster = Posters::findOne(['id' => $this->poster_id]);
             return $this->render('image',[
-                //'image' => $image ? 'images/posters/'.$image->src : $bundle->baseUrl.'/img/image.png',
                 'image' => $poster ? 'images/posters/'.$poster->image : $bundle->baseUrl.'/img/image.png',
                 'alt' => $poster ? $poster->name : 'Постер №'.$this->poster_id,
                 'img_class' => $this->img_class ? $this->img_class : null
