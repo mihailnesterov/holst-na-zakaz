9a4f01a0f1e9dfe64cde50420875744a964444e1
mihail <mhause@mail.ru>
2019-11-19 03:54:08 +0700

    add: new OrderItems model

--- models/OrderItems.php
+++ models/OrderItems.php
@@ -12,15 +12,14 @@ use Yii;
  * @property int $poster_id id постера
  * @property int $width ширина, см
  * @property int $height высота, см
- * @property int $type_id id типа постера
- * @property int $material_id id материала
- * @property double $service_1 сумма услуги №1
- * @property double $service_2 сумма услуги №2
- * @property double $service_3 сумма услуги №3
- * @property double $service_4 сумма услуги №4
- * @property double $service_5 сумма услуги №5
+ * @property string $type тип постера
+ * @property string $material материал
+ * @property int $podramnik толщина подрамника, см.
+ * @property string $add_services дополнительные услуги
  * @property int $baget_id id багета
  * @property int $clock_id id часов
+ * @property double $price цена, руб.
+ * @property int $qty количество, шт.
  * @property string $created дата создания
  *
  * @property Orders $order
@@ -41,9 +40,11 @@ class OrderItems extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['order_id', 'poster_id', 'width', 'height', 'type_id', 'material_id', 'baget_id', 'clock_id'], 'integer'],
-            [['service_1', 'service_2', 'service_3', 'service_4', 'service_5'], 'number'],
+            [['order_id', 'poster_id', 'width', 'height', 'podramnik', 'baget_id', 'clock_id', 'qty'], 'integer'],
+            [['add_services'], 'string'],
+            [['price'], 'number'],
             [['created'], 'safe'],
+            [['type', 'material'], 'string', 'max' => 255],
             [['order_id'], 'exist', 'skipOnError' => true, 'targetClass' => Orders::className(), 'targetAttribute' => ['order_id' => 'id']],
         ];
     }
@@ -59,15 +60,14 @@ class OrderItems extends \yii\db\ActiveRecord
             'poster_id' => 'Poster ID',
             'width' => 'Width',
             'height' => 'Height',
-            'type_id' => 'Type ID',
-            'material_id' => 'Material ID',
-            'service_1' => 'Service 1',
-            'service_2' => 'Service 2',
-            'service_3' => 'Service 3',
-            'service_4' => 'Service 4',
-            'service_5' => 'Service 5',
+            'type' => 'Type',
+            'material' => 'Material',
+            'podramnik' => 'Podramnik',
+            'add_services' => 'Add Services',
             'baget_id' => 'Baget ID',
             'clock_id' => 'Clock ID',
+            'price' => 'Price',
+            'qty' => 'Qty',
             'created' => 'Created',
         ];
     }
